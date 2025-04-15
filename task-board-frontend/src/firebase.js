4c931d02b2e22f7700a6c76e167fb3c77c76e336
challaharshavardhan <56510445+challaharshavardhan@users.noreply.github.com>
2025-04-14 20:43:45 -0400

    Update firebase.js
    deleted some sensitive info

--- task-board-frontend/src/firebase.js
+++ task-board-frontend/src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC23BX98KeYS3MxB-C42dMkUXVx3qxm2os",
-  authDomain: "taskauth-7b088.firebaseapp.com",
-  projectId: "taskauth-7b088",
-  storageBucket: "taskauth-7b088.firebasestorage.app",
-  messagingSenderId: "11352290268",
-  appId: "1:11352290268:web:e0fe94d3c7baf4268f5519",
-  measurementId: "G-34VZR7SFZ7"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
